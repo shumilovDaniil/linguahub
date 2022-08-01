875313da9efb031f3409bd60208850e74383cd08
shumilovDaniil <shumilov.dss@gmail.com>
2022-08-01 14:04:31 +0500

    1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: true
+};
 
-module.exports = nextConfig
+module.exports = {
+  basePath: "/linguahub",
+  assetPrefix: "/linguahub"
+};
